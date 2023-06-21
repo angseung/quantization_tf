b6aa6608d01ccc742a1906718dd8c9af2e7d6132
angseung <swim@surromind.ai>
2023-06-21 14:34:54 +0900

    Validated integer quantization.

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+import time
 import tensorflow as tf
 from tensorflow import keras
-from utils import inference
+from utils import inference, representative_dataset
 
 
 input_tensor = tf.random.uniform((1, 224, 224, 3))
 model = keras.applications.ResNet50()
 
+start_fp = time.time()
 fp_output = model(input_tensor).numpy()
+latency_fp = time.time() - start_fp
+print(f"fp32 model: {latency_fp: .4f}")
 
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 converter.optimizations = [tf.lite.Optimize.DEFAULT]  # quantization flag
+converter.representative_dataset = representative_dataset
 tflite_model_quant = converter.convert()
 
 with open("tflite/test_model.tflite", "wb") as f:
     f.write(tflite_model_quant)
 
 pred_q = inference("tflite/test_model.tflite", input_tensor)
-inference(tflite_model_quant, input_tensor)
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import os
+import time
 from typing import Union
 from pathlib import Path
+import numpy as np
 import tensorflow as tf
 
 FILE = Path(__file__).resolve()
@@ -22,6 +24,15 @@ def inference(tf_lite_model: Union[str, bytes], input_tensor: tf.Tensor) -> tf.T
     tf_lite_model.set_tensor(input_index, input_tensor)
 
     # Run inference.
+    start_qint8 = time.time()
     tf_lite_model.invoke()
+    latency_qint8 = time.time() - start_qint8
+    print(f"fp32 model: {latency_qint8: .4f}")
 
     return tf_lite_model.get_tensor(output_index)
+
+
+def representative_dataset():
+    for _ in range(10):
+        data = np.random.rand(1, 224, 224, 3)  # Calibration Data
+        yield [data.astype(np.float32)]  # Return data with generator
